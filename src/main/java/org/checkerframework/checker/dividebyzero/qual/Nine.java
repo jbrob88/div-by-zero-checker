f6174e4403fb30374627014069d6fba63c4f748d
Josh Broberg <jbrob88@cs.washington.edu>
2024-05-20 01:06:52 -0700

    Add nonzero value to lattice

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/Nine.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Nine.java
@@ -4,6 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
 
-@SubtypeOf({Top.class})
+@SubtypeOf({Nonzero.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Nine {}
